1f1f87ec24ca1167318dd775c897f972e037e31d
Christina0Athanasiadi <christina.athanasiadi1@gmail.com>
2024-08-23 16:59:21 +0200

    updates

---
+++ EEG/analysis/4. Plots.R
@@ -0,0 +1,4 @@
+setwd("C:/Users/chath1626/OneDrive - UiT Office 365/LESS-Project-with-RAs-2/EEG/analysis")
+
+source("EEG\analysis\1. LHQ3 importation script.R")
+source("EEG/analysis/Session Data importation.R")
